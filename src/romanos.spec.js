4415e2066602b6544a963ac31196a194b296d611
Alexander <alexander.vacaflores@ucb.edu.bo>
2025-02-25 03:57:47 -0400

    segundo numero prueba

--- src/romanos.spec.js
+++ src/romanos.spec.js
@@ -4,4 +4,10 @@ describe("Conversión a números romanos", () => {
   it("debería convertir 1 a 'I'", () => {
     expect(convertirARomano(1)).toEqual("I");
   });
+
+  it("debería convertir 2 a 'II'", () => {
+    expect(convertirARomano(2)).toEqual("II");
+  });
 });
+
+
